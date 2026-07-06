b364c6a0831f6eac605bdca6dfdd2bf27a6c359e
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-05 22:37:27 -0300

    fixes

--- src/flext_target_oracle/__init__.py
+++ src/flext_target_oracle/__init__.py
@@ -22,7 +22,7 @@ from flext_target_oracle._exports import (
 )
 
 if TYPE_CHECKING:
-    from flext_core._root_typing_parts import (
+    from flext_core._root_typing_parts.facades import (
         d as d,
         e as e,
         h as h,
