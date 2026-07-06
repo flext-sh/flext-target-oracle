c7e76e7eeaf5d468a32dc1f2b2aa388b68bde45b
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 02:46:41 -0300

    chore(codegen): inline lazy __init__.py, eliminate _exports.py sidecar
    Regenerated by 'flext_infra codegen init --apply': PEP 562 lazy imports are
    now inlined directly into each package __init__.py; the separate _exports.py
    lazy-map sidecar is removed (superseded by the inline map).

--- src/flext_target_oracle/__init__.py
+++ src/flext_target_oracle/__init__.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from flext_core.lazy import install_lazy_exports
+from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 from flext_target_oracle.__version__ import (
     __author__,
     __author_email__,
@@ -16,72 +16,61 @@ from flext_target_oracle.__version__ import (
     __version__,
     __version_info__,
 )
-from flext_target_oracle._exports import (
-    FLEXT_TARGET_ORACLE_LAZY_IMPORTS,
-    FLEXT_TARGET_ORACLE_PUBLIC_EXPORTS,
-)
 
 if TYPE_CHECKING:
-    from flext_db_oracle import d as d, e as e, h as h, r as r, s as s, x as x
-    from flext_target_oracle._utilities.client import (
-        FlextTargetOracle as FlextTargetOracle,
-    )
-    from flext_target_oracle.api import (
-        FlextTargetOracleService as FlextTargetOracleService,
-        target_oracle as target_oracle,
-    )
-    from flext_target_oracle.cli import (
-        FlextTargetOracleCli as FlextTargetOracleCli,
-        main as main,
-    )
-    from flext_target_oracle.constants import (
-        FlextTargetOracleConstants as FlextTargetOracleConstants,
-        c as c,
-    )
-    from flext_target_oracle.models import (
-        FlextTargetOracleModels as FlextTargetOracleModels,
-        m as m,
-    )
-    from flext_target_oracle.protocols import (
-        FlextTargetOracleProtocols as FlextTargetOracleProtocols,
-        p as p,
-    )
-    from flext_target_oracle.settings import (
-        FlextTargetOracleSettings as FlextTargetOracleSettings,
-    )
-    from flext_target_oracle.typings import (
-        FlextTargetOracleTypes as FlextTargetOracleTypes,
-        t as t,
-    )
-    from flext_target_oracle.utilities import (
-        FlextTargetOracleUtilities as FlextTargetOracleUtilities,
-        u as u,
-    )
-
-
-_LAZY_IMPORTS = {
-    name: target
-    for name, target in FLEXT_TARGET_ORACLE_LAZY_IMPORTS.items()
-    if name in FLEXT_TARGET_ORACLE_PUBLIC_EXPORTS
-}
-
-
-_EAGER_EXPORTS = (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-    __version_info__,
+    from flext_db_oracle import d, e, h, r, s, x
+    from flext_target_oracle.api import FlextTargetOracleService
+    from flext_target_oracle.cli import FlextTargetOracleCli, main
+    from flext_target_oracle.constants import FlextTargetOracleConstants, c
+    from flext_target_oracle.models import FlextTargetOracleModels, m
+    from flext_target_oracle.protocols import FlextTargetOracleProtocols, p
+    from flext_target_oracle.settings import FlextTargetOracleSettings
+    from flext_target_oracle.typings import FlextTargetOracleTypes, t
+    from flext_target_oracle.utilities import FlextTargetOracleUtilities, u
+_LAZY_IMPORTS = build_lazy_import_map(
+    {
+        ".api": (
+            "FlextTargetOracleService",
+            "target_oracle",
+        ),
+        ".cli": (
+            "FlextTargetOracleCli",
+            "main",
+        ),
+        ".constants": (
+            "FlextTargetOracleConstants",
+            "c",
+        ),
+        ".models": (
+            "FlextTargetOracleModels",
+            "m",
+        ),
+        ".protocols": (
+            "FlextTargetOracleProtocols",
+            "p",
+        ),
+        ".settings": ("FlextTargetOracleSettings",),
+        ".typings": (
+            "FlextTargetOracleTypes",
+            "t",
+        ),
+        ".utilities": (
+            "FlextTargetOracleUtilities",
+            "u",
+        ),
+        "flext_db_oracle": (
+            "d",
+            "e",
+            "h",
+            "r",
+            "s",
+            "x",
+        ),
+    },
 )
 
 
-_PUBLIC_EXPORTS: tuple[str, ...] = FLEXT_TARGET_ORACLE_PUBLIC_EXPORTS
-
 __all__: tuple[str, ...] = (
-    "FlextTargetOracle",
     "FlextTargetOracleCli",
     "FlextTargetOracleConstants",
     "FlextTargetOracleModels",
@@ -118,5 +107,5 @@ install_lazy_exports(
     __name__,
     globals(),
     _LAZY_IMPORTS,
-    public_exports=_PUBLIC_EXPORTS,
+    public_exports=__all__,
 )
