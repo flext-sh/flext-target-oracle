70f18a3267a3460cd35468c2ad29ca03cafc9d45
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-08 16:09:22 -0300

    fixes

--- src/flext_target_oracle/__init__.py
+++ src/flext_target_oracle/__init__.py
@@ -20,7 +20,8 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_db_oracle import FlextDbOracleConstants, d, e, h, r, s, x
+    from flext_db_oracle import FlextDbOracleConstants
+    from flext_meltano import d, e, h, r, s, x
 
     from ._config import FlextTargetOracleConfig, config
     from ._settings import FlextTargetOracleSettings, settings
@@ -91,7 +92,8 @@ _LAZY_IMPORTS = MappingProxyType(
                 "FlextTargetOracleUtilities",
                 "u",
             ),
-            "flext_db_oracle": ("FlextDbOracleConstants", "d", "e", "h", "r", "s", "x"),
+            "flext_db_oracle": ("FlextDbOracleConstants",),
+            "flext_meltano": ("d", "e", "h", "r", "s", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
