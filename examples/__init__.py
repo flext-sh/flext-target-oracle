1f9792b95834b74417194637af1f238b98cbe92c
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 12:30:08 -0300

    chore(gen): converge lazy-init projections after identifier filter

--- examples/__init__.py
+++ examples/__init__.py
@@ -9,26 +9,31 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_target_oracle import (
-        FlextTargetOracleConstants,
-        FlextTargetOracleConstants as c,
-        d,
-        e,
-        h,
-        m,
-        p,
-        r,
-        s,
-        t,
-        u,
-        x,
-    )
+    from flext_db_oracle import e, s
 
-    from .constants import ExamplesFlextTargetOracleConstants
-    from .models import ExamplesFlextTargetOracleModels
-    from .protocols import ExamplesFlextTargetOracleProtocols
-    from .typings import ExamplesFlextTargetOracleTypes
-    from .utilities import ExamplesFlextTargetOracleUtilities
+    from flext_core import d, h, r, x
+    from flext_target_oracle import FlextTargetOracleConstants
+
+    from .constants import (
+        ExamplesFlextTargetOracleConstants,
+        ExamplesFlextTargetOracleConstants as c,
+    )
+    from .models import (
+        ExamplesFlextTargetOracleModels,
+        ExamplesFlextTargetOracleModels as m,
+    )
+    from .protocols import (
+        ExamplesFlextTargetOracleProtocols,
+        ExamplesFlextTargetOracleProtocols as p,
+    )
+    from .typings import (
+        ExamplesFlextTargetOracleTypes,
+        ExamplesFlextTargetOracleTypes as t,
+    )
+    from .utilities import (
+        ExamplesFlextTargetOracleUtilities,
+        ExamplesFlextTargetOracleUtilities as u,
+    )
 __all__: tuple[str, ...] = (
     "ExamplesFlextTargetOracleConstants",
     "ExamplesFlextTargetOracleModels",
@@ -52,25 +57,14 @@ __all__: tuple[str, ...] = (
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            ".constants": ("ExamplesFlextTargetOracleConstants",),
-            ".models": ("ExamplesFlextTargetOracleModels",),
-            ".protocols": ("ExamplesFlextTargetOracleProtocols",),
-            ".typings": ("ExamplesFlextTargetOracleTypes",),
-            ".utilities": ("ExamplesFlextTargetOracleUtilities",),
-            "flext_target_oracle": (
-                "FlextTargetOracleConstants",
-                "c",
-                "d",
-                "e",
-                "h",
-                "m",
-                "p",
-                "r",
-                "s",
-                "t",
-                "u",
-                "x",
-            ),
+            ".constants": ("ExamplesFlextTargetOracleConstants", "c"),
+            ".models": ("ExamplesFlextTargetOracleModels", "m"),
+            ".protocols": ("ExamplesFlextTargetOracleProtocols", "p"),
+            ".typings": ("ExamplesFlextTargetOracleTypes", "t"),
+            ".utilities": ("ExamplesFlextTargetOracleUtilities", "u"),
+            "flext_core": ("d", "h", "r", "x"),
+            "flext_db_oracle": ("e", "s"),
+            "flext_target_oracle": ("FlextTargetOracleConstants",),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
--- src/flext_target_oracle/__init__.py
+++ src/flext_target_oracle/__init__.py
@@ -20,7 +20,9 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_db_oracle import FlextDbOracleConstants, d, e, h, r, s, x
+    from flext_db_oracle import e, s
+
+    from flext_core import d, h, r, x
 
     from .__version__ import FlextTargetOracleVersion
     from ._config import FlextTargetOracleConfig, config
@@ -39,7 +41,6 @@ if TYPE_CHECKING:
         FlextTargetOracleUtilities as u,
     )
 __all__: tuple[str, ...] = (
-    "FlextDbOracleConstants",
     "FlextTargetOracle",
     "FlextTargetOracleCli",
     "FlextTargetOracleConfig",
@@ -97,7 +98,8 @@ _LAZY_IMPORTS = MappingProxyType(
                 "FlextTargetOracleUtilities",
                 "u",
             ),
-            "flext_db_oracle": ("FlextDbOracleConstants", "d", "e", "h", "r", "s", "x"),
+            "flext_core": ("d", "h", "r", "x"),
+            "flext_db_oracle": ("e", "s"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
