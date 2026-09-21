0b7ec3364d92f23c5c3fc9ec2cf2285e8e50ced9
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 13:45:20 -0300

    chore(gen): converge projections with the published generator (d1c55b54f, one name per line)

--- src/flext_target_oracle/__init__.py
+++ src/flext_target_oracle/__init__.py
@@ -20,9 +20,8 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_db_oracle import e, s
-
     from flext_core import d, h, r, x
+    from flext_db_oracle import e, s
 
     from .__version__ import FlextTargetOracleVersion
     from ._config import FlextTargetOracleConfig, config
